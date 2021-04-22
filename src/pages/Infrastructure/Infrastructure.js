01195e0b1043b8a7b7013e78cdeb9cb9324107e1
yesh_24 <yesh242@users.github.com>
2021-04-22 13:12:37 +0530

    head

--- src/pages/Infrastructure/Infrastructure.js
+++ src/pages/Infrastructure/Infrastructure.js
@@ -1,11 +1,24 @@
 import React from "react";
+import './Infrastructure.css'
 
 const Infrastructure = () => {
   return (
-    <div>
-      <h2>This is Infrastructure Page</h2>
+    <div className="Home">
+      <div className="lander">
+        <h1>We are a recognized leader in high-tech ophthalmology services</h1>
+        <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut
+labore et dolore magna aliqua.</p></div>
+        <div className="image">
+          <img src={require('./assets/image.png').default} alt="first pic" />
+        </div>
+        <div className='body'>
+          <p>Next topic</p>
+        </div>
     </div>
   );
 };
 
+
 export default Infrastructure;
+
+

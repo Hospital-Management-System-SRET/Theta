470e1a4be68cb1d11dd5c5788b4ef37c98d4199c
yesh_24 <yesh242@users.github.com>
2021-05-01 13:23:30 +0530

    second changes

--- src/pages/Infrastructure/Infrastructure.js
+++ src/pages/Infrastructure/Infrastructure.js
@@ -2,6 +2,8 @@ import React from "react";
 import './Infrastructure.css'
 import Toggle from "./Toggle/Toggle";
 
+
+
 const Infrastructure = (props) => {
 
   return (
@@ -22,7 +24,25 @@ labore et dolore magna aliqua.</p></div>
 
       <div className='end'>
         <h2>Our Workspace</h2>
+        <br></br>
+        {/* <div class="outer-grid">
+        <div class="inner-grid">
+            <img src={require('./assets/eyec.jpg').default} alt="Snow" width="550" height="400"/>
 
+            <img src={require('./assets/child.jpg').default} alt="Snow" width="550" height="400"/>
+
+            <img src={require('./assets/left.jpg').default} alt="Snow" width="550" height="400"/>
+          </div>
+        </div> */}
+        <h2>
+        The face is a picture of the mind with an eyes
+        <p>Have questions?</p>
+        <p>(123) 456-7890</p>
+        </h2>
+  
+      </div>
+      <div className = "footer">
+        
       </div>
     </div>
   );
--- src/pages/Infrastructure/Toggle/Toggle.js
+++ src/pages/Infrastructure/Toggle/Toggle.js
@@ -11,46 +11,57 @@ class Toggle extends Component {
         return (
             <section id="content">
                 <div className="container">
-                    <div className = 'doc'>
+                    <div>
                         <button onClick={this.showHide} className="tog">WHO WE ARE</button>
                         {this.state.show &&
-                            <div>
-                                <img src={require('../assets/lens.jpg').default} alt="loading" ></img>
-                                <img src={require('../assets/check.jpg').default} alt="loading" ></img>
+                            <div className = "doc">
+                                
+                                <div className = "secondimg">
+                                <img clasName = "imageone" src={require('../assets/read.jpg').default} alt="loading" width="260" height="260"></img>
+                                <img className = "image" src={require('../assets/check.jpg').default} alt="loading" width="260" height="195"></img>
+                                </div>
+
+                                <div className = "firstimg" >
+                                <img className = "lens" src={require('../assets/lens.jpg').default} alt="loading" width="160" height="260"></img>
+                                </div>
+                                
                                 <div className='context'>
-                                <h2>We are a recognized leader in high-tech ophthalmology services</h2>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </p>
-                                <p>
-                                    Ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Nam aliquam sem et tortor consequat id porta.
-                                    In metus vulputate eu scelerisque felis imperdiet.Ut etiam sit amet nisl purus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquet lectus proin nibh nisl condimentum id venenatis a condimentum.
+                                    <h2>We are a recognized leader in high-tech ophthalmology services</h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </p>
+                                    <p>
+                                        Ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Nam aliquam sem et tortor consequat id porta.
+                                        In metus vulputate eu scelerisque felis imperdiet.Ut etiam sit amet nisl purus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquet lectus proin nibh nisl condimentum id venenatis a condimentum.
                                 </p>
+                                    <div className='btn'>
+                                        <button>Our Doctors</button>
+                                    </div>
                                 </div>
-                                <div className = 'btn'>
-                                    <button>Our Doctors</button>
-                                </div>
+
                             </div>
                         }
                     </div>
                     <br></br>
                     <br></br>
                     <br></br>
-                    
-                    <div>
+
+                    <div className="second">
                         <button onClick={this.showHide} className="tog">ADVANTAGES</button>
                         {this.state.show &&
-                            <div className= "col">
+                            <div className="col">
                                 <h2>We offer a whole range of treatments for you and your family.</h2>
                                 <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Nam aliquam sem et tortor consequat id porta. In metus vulputate eu scelerisque felis imperdiet.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Nam aliquam sem et tortor consequat id porta. In metus vulputate eu scelerisque felis imperdiet.
                                 </p>
                                 <p>
-                                Ut etiam sit amet nisl purus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquet lectus proin nibh nisl condimentum id venenatis a condimentum.
+                                    Ut etiam sit amet nisl purus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquet lectus proin nibh nisl condimentum id venenatis a condimentum.
                                 </p>
-                                <div className = 'btn'>
+                                <div className='btn'>
                                     <button>Book Appointment</button>
                                 </div>
-                            </div>  
+                                <br></br>
+                            </div>
+
                         }
                     </div>
                 </div>
@@ -67,46 +78,3 @@ export default Toggle;
 
 
 
-// import React from 'react';
-// import './Toggle.css';
-
-// class Toggle extends React.Component{
-//   constructor(){
-//       super();
-//       this.state={
-//           show:true
-//       }
-//   }
-//   render(){
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//         <button className='tog' onClick={()=>{this.setState({show:!this.state.show})}}>
-//                   { this.state.show? 'Hide' : 'WHO WE ARE'}</button>
-//           <div>
-//               {
-//                   this.state.show? 
-//                   <div className= 'doc'>
-//                       <img src={require('../assets/lens.jpg').default} alt="loading" ></img>
-//                       <img src={require('../assets/check.jpg').default} alt="loading" ></img>
-//                       <img src={require('../assets/read.jpg').default} alt="loading" ></img>
-//                       <h2>We are a recognized leader in high-tech ophthalmology services</h2>
-//                       <p>
-//                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </p>
-//                       <p>
-//                       Ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Nam aliquam sem et tortor consequat id porta.
-//                       In metus vulputate eu scelerisque felis imperdiet.Ut etiam sit amet nisl purus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquet lectus proin nibh nisl condimentum id venenatis a condimentum.
-//                       </p>
-//                       <div className = 'btn-grp-1'>
-//                           <button>Our Doctors</button>
-//                       </div>
-//                     </div> : null
-//               }
-//           </div>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Toggle;
